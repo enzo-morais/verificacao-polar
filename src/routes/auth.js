6d91899fc353c078ef2f3499eeac57fd60767bc4
VOID Systems <void@systems.dev>
2026-05-05 15:23:26 -0300

    fix: adiciona rota /auth/callback para o redirect do Discord

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/auth/discord', authController.authRedirect);
 router.get('/callback', authController.callback);
+router.get('/auth/callback', authController.callback);
 
 module.exports = router;
